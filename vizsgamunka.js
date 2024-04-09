6294e3f939c5a5aa894e97448a6de991b3737418
“[szajkojeno]” <[szajkojeno6@gmail.com]>
2024-04-09 18:20:50 +0200

    img has modified, index.html has modified, css has modified, vizsgamunka.js ha been created

---
+++ vizsgamunka.js
@@ -0,0 +1,68 @@
+$(document).ready(function () {
+  $("#monsterCM").click(function () {
+
+    $("#monsterszovegCM").slideToggle();
+    
+    $("#monsterszovegCM").css("display", "inline-block", "text-align", "center").css("display", "inline-block", "text-align", "center").animate({ paddinBottom: "100px", });
+
+
+  });
+
+
+  $("#monsterNT").click(function () {
+    $("#monsterszovegNT").slideToggle();
+
+    $("#monsterszovegNT").css("display", "inline-block", "text-align", "center").animate({ paddinBottom: "100px", });
+  });
+
+
+
+
+
+  $("#monsterZCM").click(function () {
+    $("#monsterszovegZCM").slideToggle();
+
+    $("#monsterszovegZCM").css("display", "inline-block", "text-align", "center").animate({ paddinBottom: "100px", });
+  });
+
+
+  $("#monsterUF").click(function () {
+    $("#monsterszovegUF").slideToggle();
+
+    $("#monsterszovegUF").css("display", "inline-block", "text-align", "center").animate({ paddinBottom: "100px", });
+  });
+
+
+  $("#monsterUP").click(function () {
+    $("#monsterszovegUP").slideToggle();
+
+    $("#monsterszovegUP").css("display", "inline-block", "text-align", "center").animate({ paddinBottom: "100px" });
+  });
+
+
+  $("#monsterUV").click(function () {
+    $("#monsterszovegUV").slideToggle();
+
+    $("#monsterszovegUV").css("display", "inline-block", "text-align", "center").animate({ paddinBottom: "100px" });
+  });
+
+
+  $("#monsterRC").click(function () {
+    $("#monsterszovegRC").slideToggle();
+
+
+    $("#monsterszovegRC").css("display", "inline-block", "text-align", "center").animate({ paddinBottom: "100px"});
+  });
+
+
+  $("#monsterUM").click(function () {
+    $("#monsterszovegUM").slideToggle();
+
+        
+        $("#monsterszovegUM").css("display", "inline-block", "text-align", "center",).animate({ paddinBottom: "100px"});
+    });
+
+
+
+
+  });
